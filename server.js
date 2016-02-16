edc07ceb04bf5126c542c8c157a666a86ca51334
Thomas Holley <thomas.holley@att.net>
2016-02-16 17:51:33 -0600

    Added PUT to API

--- server.js
+++ server.js
@@ -71,6 +71,39 @@ app.delete('/todos/:id', function (req, res) {
 	}
 });
 
+// PUT /todos/:id
+
+app.put('/todos/:id', function (req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodoId = _.findWhere(todos, {id: todoId});	
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTodoId) {
+		return res.status(404).send();
+	}
+
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	}else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	}else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodoId, validAttributes);
+	res.json(matchedTodoId);
+
+
+});
+
+
 app.listen(PORT, function (){
 	console.log('Express listening on port ' + PORT + '!');
 });
