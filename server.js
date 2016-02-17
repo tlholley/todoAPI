6bebd039baabab05cfc6413b2269a4700040a9e0
Thomas Holley <thomas.holley@att.net>
2016-02-17 11:23:48 -0600

    Added Query to API

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ app.get('/', function(req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+	
+	res.json(filteredTodos);
 });
 // GET /todos/:id
 app.get('/todos/:id', function (req,res) {
@@ -73,7 +82,7 @@ app.delete('/todos/:id', function (req, res) {
 
 // PUT /todos/:id
 
-app.put('/todos/:id', function (req, res){
+app.put('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodoId = _.findWhere(todos, {id: todoId});	
 	var body = _.pick(req.body, 'description', 'completed');
