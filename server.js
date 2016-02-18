d160d1797533d748958ce3b033c189a0defc0d7e
Thomas Holley <thomas.holley@att.net>
2016-02-18 17:28:09 -0600

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,42 +16,73 @@ app.get('/', function(req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
-		filteredTodos = _.where(filteredTodos, {completed: false});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query. hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 
-	if(queryParams.hasOwnProperty('q')  && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function (todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) > -1;
-		});
+	if (query.hasOwnProperty('q')  && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then(function (todos) {
+		res.json(todos);
+	}, function (e) {
+		res.status(500).send();
+	});
+
+	// var filteredTodos = todos;
+
+	// if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+	// 	filteredTodos = _.where(filteredTodos, {completed: true});
+	// }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+	// 	filteredTodos = _.where(filteredTodos, {completed: false});
+	// }
+
+	// if(queryParams.hasOwnProperty('q')  && queryParams.q.length > 0) {
+	// 	filteredTodos = _.filter(filteredTodos, function (todo) {
+	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) > -1;
+	// 	});
+	// }
+
+	// res.json(filteredTodos);
 });
 // GET /todos/:id
 app.get('/todos/:id', function (req,res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodoId = _.findWhere(todos, {id: todoId});
-	// Changed to underscore ----------------------
-	// var matchedTodoId;
 
-	// todos.forEach(function (todo) {
-	// 	if (todoId === todo.id) {
-	// 		matchedTodoId = todo;
-	// 	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
 
-	// });
+	// var matchedTodoId = _.findWhere(todos, {id: todoId});
+	// // Changed to underscore ----------------------
+	// // var matchedTodoId;
 
-	if(matchedTodoId) {
-		res.json(matchedTodoId);		
-	}else {
-		res.status(404).send();
-	}
+	// // todos.forEach(function (todo) {
+	// // 	if (todoId === todo.id) {
+	// // 		matchedTodoId = todo;
+	// // 	}
+
+	// // });
+
+	// if(matchedTodoId) {
+	// 	res.json(matchedTodoId);		
+	// }else {
+	// 	res.status(404).send();
+	// }
 });
 
 // POST /todos
