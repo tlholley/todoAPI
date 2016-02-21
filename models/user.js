dec8afdc43e2570a7cf31bcd99bb7b9a28ef3be4
Thomas Holley <thomas.holley@att.net>
2016-02-21 15:58:33 -0600

    Add auth middleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -63,20 +63,42 @@ module.exports = function (sequelize, DataTypes) {
 						reject();
 					});
 				});
+			},
+			findByToken: function (token) {
+				return new Promise(function (resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function (user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function(e) {
+							reject();
+						});
+					} catch (e) {
+						reject();
+					}
+
+				});
 			}
 		},
 		instanceMethods: {
 			toPublicJSON: function () {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-			},generateToken: function (type) {
+			},			generateToken: function (type) {
 				if (!_.isString(type)) {
 					return undefined;
 				}
 
 				try {
 					var stringData = JSON.stringify({id: this.get('id'), type: type});
-					var encryptedData = cryptojs.AES.encrypt(stringData, "abc123!@#").toString();
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#').toString();
 					var token = jwt.sign({
 						token: encryptedData
 					}, 'qwerty098');
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcryptjs = require('bcryptjs');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ app.get('/', function(req, res) {
 });
 
 // GET /todos
-app.get('/todos', function (req, res) {
+app.get('/todos', middleware.requireAuthentication, function (req, res) {
 	var query = req.query;
 	var where = {};
 
@@ -55,7 +56,7 @@ app.get('/todos', function (req, res) {
 	// res.json(filteredTodos);
 });
 // GET /todos/:id
-app.get('/todos/:id', function (req,res) {
+app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.findById(todoId).then(function (todo) {
@@ -87,7 +88,7 @@ app.get('/todos/:id', function (req,res) {
 });
 
 // POST /todos
-app.post('/todos', function (req, res) {
+app.post('/todos', middleware.requireAuthentication, function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	
 	db.todo.create(body).then(function (todo){
@@ -111,7 +112,7 @@ app.post('/todos', function (req, res) {
 
 // DELETE /todos/:id
 
-app.delete('/todos/:id', function (req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -142,7 +143,7 @@ app.delete('/todos/:id', function (req, res) {
 
 // PUT /todos/:id
 
-app.put('/todos/:id', function (req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	// var matchedTodoId = _.findWhere(todos, {id: todoId});	
 	var body = _.pick(req.body, 'description', 'completed');
