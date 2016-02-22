7ac3acb4563a5ec156d5f31288ae0c192953b37d
Thomas Holley <thomas.holley@att.net>
2016-02-22 11:46:53 -0600

    Added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,16 +19,49 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
-sequelize.sync().then(function () {
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+	// force: true
+}).then(function () {
 	console.log('Everything is synced.');
 
-	Todo.findById(1).then(function (todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('Todo not found!');
-		}
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function (todos) {
+			todos.forEach (function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
 	});
+
+	// User.create({
+	// 	email: 'thomas@example.com'
+	// }).then(function () {
+	// 	return Todo.create({
+	// 		description: 'clean yard'
+	// 	});
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
+	// 	});
+	// });
+
+	// Todo.findById(1).then(function (todo) {
+	// 	if (todo) {
+	// 		console.log(todo.toJSON());
+	// 	} else {
+	// 		console.log('Todo not found!');
+	// 	}
+	// });
 
 	// Todo.create({
 	// 	description: 'Take out trash',
