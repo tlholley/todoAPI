9e1b10ac5b48cb13bc6ed91b4be02ab932491d4a
Thomas Holley <thomas.holley@att.net>
2016-02-22 12:06:49 -0600

    Convert CRUD req to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res) {
 // GET /todos
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -59,7 +61,12 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function (todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function (todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -122,7 +129,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function (rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -192,7 +200,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	// }, function (e) {
 	// 	res.status(400).json(e);
 	// });
-	db.todo.findById(todoId).then(function (todo){
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function (todo){
 		if (todo) {
 			todo.update(attributes).then(function (todo) {
 		res.json(todo.toJSON());
